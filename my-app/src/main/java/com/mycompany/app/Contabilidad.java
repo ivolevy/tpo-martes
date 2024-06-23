15ae0ca4d5d1eea4e3f08d4633730489d43a8c07
ivan levy <ivo.levy03@gmail.com>
2024-06-22 21:02:40 -0300

    a

--- my-app/src/main/java/com/mycompany/app/Contabilidad.java
+++ my-app/src/main/java/com/mycompany/app/Contabilidad.java
@@ -3,7 +3,9 @@ package com.mycompany.app;
 import java.util.List;
 
 public class Contabilidad {
+    @SuppressWarnings("unused")
     private List<Reserva> reservas;
+    @SuppressWarnings("unused")
     private List<Factura> facturas;
 
     public void generarFacturaReserva(Reserva reserva, Factura factura) {
